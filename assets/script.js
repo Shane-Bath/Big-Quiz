6805acbe57613db616e2784b5f57619fdc9c2fc3
Shane-Bath <shane.bath@gmail.com>
2023-01-31 22:27:40 +0000

    Add rough CSS styles to page

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 const game = document.getElementById('game')
 const gamebtn = document.getElementById('btn')
 
+
 function element() {
     const h1 = document.createElement('h1');
     h1.innerHTML = 'I am a H1 Tag';
@@ -21,3 +22,15 @@ function element() {
 }
 element();
 
+function gamearea() {
+    const board = document.createElement('div');
+    board.setAttribute('id', 'main-game')
+    if (board.style.display === 'none'){
+        board.style.display = 'block';
+    } else {
+    board.innerHTML= 'this is what to you see if you click the button';
+    document.body.appendChild(board)
+    }
+}
+
+gamearea();
