9fbec828ad55dcf5eba54e18de8a86f2a08e0ef2
Shane-Bath <shane.bath@gmail.com>
2023-02-01 21:36:45 +0000

    Add fetch function to retrieve question from opentdb.com

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
-const game = document.getElementById('game')
-const gamebtn = document.getElementById('btn')
+const game = document.getElementById('game');
+const gamebtn = document.getElementById('btn');
+const  category = [9, 10, 11];
 
 
 function element() {
@@ -15,7 +16,7 @@ function element() {
 
     const button = document.createElement("BUTTON");
     button.setAttribute('id', "btn-btn");
-    button.innerHTML = ' Press Here'
+    button.innerHTML = 'START'
     //document.body.appendChild(button);
     game.append(button);
 
@@ -26,8 +27,20 @@ function gamearea() {
     const board = document.createElement('div');
     board.setAttribute('id', 'main-game')
     board.innerHTML= 'this is what to you see if you click the button';
-    document.body.appendChild(board)
+    document.body.appendChild(board);
 
+    const question = document.createElement('h3')
+    question.setAttribute('class', 'question');
+    question.data();
+    board.append(question);
+    
+    category.forEach(category => {
+        fetch(`https://opentdb.com/api.php?amount=1&category=${category}&difficulty=easy&type=boolean`)
+            .then(response => response.json())
+            .then(data => console.log(data))
+    });
 }
 
 gamearea();
+
+
