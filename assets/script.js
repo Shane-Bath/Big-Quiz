10eea92873ec10f3b2c75d65555f23405c240c9c
Shane-Bath <shane.bath@gmail.com>
2023-02-09 00:46:03 +0000

    add edit to fetch to remove undifined console.log(setQuestion)

--- assets/script.js
+++ assets/script.js
@@ -20,27 +20,39 @@ start the game, https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/hid
    let totalQuestions = 3;
 
    
+//https://stackoverflow.com/questions/684672/how-do-i-loop-through-or-enumerate-a-javascript-object?noredirect=1&lq=1
+//https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+//https://stackoverflow.com/questions/29886552/why-are-objects-not-iterable-in-javascript
+//https://www.freecodecamp.org/news/javascript-array-of-objects-tutorial-how-to-create-update-and-loop-through-objects-using-js-array-methods/
+//https://stackoverflow.com/questions/69312775/how-do-i-populate-an-array-of-objects-where-every-object-has-an-array-inside-of
+//https://stackoverflow.com/questions/586182/how-to-insert-an-item-into-an-array-at-a-specific-index-javascript?rq=1
+//https://stackoverflow.com/questions/1996747/add-new-value-to-an-existing-array-in-javascript?noredirect=1&lq=1
+//https://stackoverflow.com/questions/64121914/pushing-fetched-data-to-array-of-objects
+//https://stackoverflow.com/questions/586182/how-to-insert-an-item-into-an-array-at-a-specific-index-javascript?rq=1
+//https://stackoverflow.com/questions/68123766/javascript-how-to-push-fetch-data-to-an-array
+//https://www.youtube.com/watch?v=uxf0--uiX0I
+//https://www.youtube.com/watch?v=tc8DU14qX6I&t=10s
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+//https://www.youtube.com/watch?v=zgHim4ZDpZY&list=PLJJVMUtpCkXWgmK8ksQGZuhlHXbG2Rlyn&index=21&t=3150s     10:00  1200 26:13
+
 
    
-    fetch(`https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=boolean`)
+    fetch(`https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`)
     .then(response => response.json())
     .then(data => {
-        let a = [data];
-        let setQuestions =[];
-        setQuestions = setQuestions.concat(a);
-        console.log(setQuestions);
+        console.log(data.results); // returning array
+        data.results.map(data => {
+            let setQuestions = {
+                question: data.question   
+            };
+            console.log(setQuestions)  // returning question: question
         
-       
-    })
+        let options = [... data.incorrect_answers]; // array spread operator to divide up into a new array
+        options.splice(setQuestions.options +1, 0, data.incorrect_answers); // splice into options1: etc
+        console.log(options);  // returning mix of  'M&amp;M&#039;s' ?!? and clear text?
+    });
+})
     .catch(error => console.error(error));
-    
-
-  
-
-
-   
-   
-
 
 
 //    let questionTransfer = 
@@ -216,7 +228,7 @@ options.forEach(options => {
     });
 });
 
-//startGame();
+startGame();
 
 //endGame.HMTL
 //let finalScore = document.getElementById("final-score")
